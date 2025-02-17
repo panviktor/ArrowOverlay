59d08ed03b9073bf98a622ee44f167e249b0c619
Viktor <panafviktor@gmail.com>
2025-02-17 17:01:18 +0300

    Initial Commit

---
+++ ArrowOverlay/ArrowOverlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArrowOverlayApp.swift
+//  ArrowOverlay
+//
+//  Created by Viktor on 17.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ArrowOverlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
